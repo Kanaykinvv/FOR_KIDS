66db548d32ec7f18073a20044e0b70468a283f4d
Victor Kanaykin <Kanaykinvv@mail.ru>
2022-03-27 22:47:56 +0300

    create 0-files

--- create_files.py
+++ create_files.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+import os.path
 import sys
 import argparse
 
 
 def createParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output')
+    parser.add_argument('-o', '--output', default="./", type=str)
     parser.add_argument('-c', '--count', default=1, type=int)
 
     return parser
 
+def create_file(count: int, output: str):
+    for i in range(count):
+        with open(output + "test_file_%s" % i, "w") as file:
+            file.write("0"*100)
 
 if __name__ == '__main__':
     parser = createParser()
@@ -19,6 +23,6 @@ if __name__ == '__main__':
     if args.output:
         print("output = " + args.output)
     if args.count:
-        print("Count = " + str(args.count))
+        print("count = " + str(args.count))
+    create_file(args.count, args.output)
 
-    print(args)
